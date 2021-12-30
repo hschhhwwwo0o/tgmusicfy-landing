ebd3c3eeb3b93a1da896a83ccac004fea0f4031d
Saveliy Andronov <ssandry.github@gmail.com>
2021-12-30 14:05:50 +0300

    Add title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>TGMusicfy</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap" rel="stylesheet"></link>
